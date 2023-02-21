04af30ed809b7d14c299ad7027487e44402ea0eb
Sanjay <sanjaybishtamazon@gmail.com>
2023-02-22 04:08:56 +0530

    Initial Setup of the Review 7 Star Project

---
+++ src/main/java/com/review/SevenStarsApplication.java
@@ -0,0 +1,12 @@
+package com.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenStarsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SevenStarsApplication.class, args);
+        System.out.println("Application Started");
+    }
+}
